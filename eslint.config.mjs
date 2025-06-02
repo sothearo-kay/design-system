99001482f9d3ca6b5763abf98cf534b52686d458
sothearo-kay <sothearo.kay@techostartup.center>
2025-06-02 15:34:05 +0700

    fix: fixed eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,24 @@
-import nuxtEslintConfig from '@nuxt/eslint-config';
+import { createConfigForNuxt } from '@nuxt/eslint-config';
 
-export default {
-  root: true,
-  extends: [nuxtEslintConfig],
-  ignorePatterns: ['dist', 'node_modules', '.output', '.nuxt'],
-  rules: {
-    // global
-    quotes: ['error', 'single'],
-    'quote-props': ['error', 'as-needed'],
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-
-    // vue
-    indent: ['error', 2],
-    'vue/max-attributes-per-line': 'off',
-    'vue/multi-word-component-names': 0,
-    'vue/no-v-html': 0
-  }
-};
+export default createConfigForNuxt()
+  .prepend({
+    ignores: ['dist', 'node_modules', '.output', '.nuxt']
+  })
+  .append(
+    {
+      rules: {
+        quotes: ['error', 'single'],
+        'quote-props': ['error', 'as-needed'],
+        'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        indent: ['error', 2]
+      }
+    },
+    {
+      files: ['**/*.vue'],
+      rules: {
+        'vue/max-attributes-per-line': 'off',
+        'vue/multi-word-component-names': 0,
+        'vue/no-v-html': 0
+      }
+    }
+  );
