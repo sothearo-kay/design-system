4987a9567840c855ad774022584c2ac414f36f57
sothearo-kay <sothearo.kay@techostartup.center>
2025-06-03 23:48:50 +0700

    feat(token): add typograhpy

--- panda.config.ts
+++ panda.config.ts
@@ -10,7 +10,35 @@ export default defineConfig({
   ],
   exclude: [],
   theme: {
-    extend: {}
+    extend: {
+      tokens: {
+        fontSizes: {
+          xs: { value: '0.75rem' },
+          sm: { value: '0.875rem' },
+          md: { value: '1rem' },
+          lg: { value: '1.125rem' },
+          xl: { value: '1.25rem' },
+          '2xl': { value: '1.5rem' },
+          '3xl': { value: '1.875rem' },
+          '4xl': { value: '2.25rem' },
+          '5xl': { value: '3rem' }
+        },
+        fontWeights: {
+          normal: { value: '400' },
+          medium: { value: '500' },
+          semibold: { value: '600' },
+          bold: { value: '700' }
+        },
+        lineHeights: {
+          none: { value: '1' },
+          tight: { value: '1.25' },
+          snug: { value: '1.375' },
+          normal: { value: '1.5' },
+          relaxed: { value: '1.625' },
+          loose: { value: '2' }
+        }
+      }
+    }
   },
   outdir: 'styled-system'
 });
